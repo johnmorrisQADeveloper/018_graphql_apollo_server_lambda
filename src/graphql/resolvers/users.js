bdba83fcd99875178e6b87ecb70bb920caaa5bdd
john morris <fennymorris@live.co.uk>
2020-06-16 14:16:03 +0100

    add logic to check if user exists

--- src/graphql/resolvers/users.js
+++ src/graphql/resolvers/users.js
@@ -2,12 +2,21 @@ const User = require('../../models/User')
 const bcrypt = require('bcryptjs')
 const jwt = require('jsonwebtoken')
 const { SECRET_KEY } = require('../../config')
+const { UserInputError } = require('apollo-server-lambda')
 module.exports = {
   Mutation: {
     async register (parent, { registerInput: { username, email, password, confirmPassword } }, context, info) {
       // TODO: validate user data
       // TODO: make sure user doesn't already exists
-      // TODO: hash password and create an auth token
+      const user = await User.findOne({ username })
+      if (user) {
+        throw new UserInputError('user already exists', {
+          errors: {
+            username: 'This username is taken'
+          }
+        })
+      }
+      // DONE: hash password and create an auth token
       password = await bcrypt.hash(password, 12)
       const newUser = new User({
         email,
