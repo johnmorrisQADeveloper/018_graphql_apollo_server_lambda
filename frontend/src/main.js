ed8f2dd0d948a1bd4c20c364eef2822599db6ef4
john morris <fennymorris@live.co.uk>
2020-06-18 17:50:44 +0100

    setup apolo client and setup for apollo graphql

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,12 +1,36 @@
 import Vue from 'vue'
-import App from './App.vue'
+import VueApollo from 'vue-apollo'
+import { ApolloClient } from 'apollo-client'
+import { createHttpLink } from 'apollo-link-http'
+import { InMemoryCache } from 'apollo-cache-inmemory'
+
 import router from './router'
 import store from './store'
+import App from './App.vue'
 
+const httpLink = createHttpLink({
+  uri: 'http://localhost:3000/graphql'
+})
+
+const apolloClient = new ApolloClient({
+  link: httpLink,
+  cache: new InMemoryCache(),
+  connectToDevTools: true
+})
+
+const apolloProvider = new VueApollo({
+  defaultClient: apolloClient,
+  defaultOptions: {
+    $loadingKey: 'loading'
+  }
+})
+
+Vue.use(VueApollo)
 Vue.config.productionTip = false
 
 new Vue({
   router,
   store,
+  apolloProvider,
   render: h => h(App)
 }).$mount('#app')
