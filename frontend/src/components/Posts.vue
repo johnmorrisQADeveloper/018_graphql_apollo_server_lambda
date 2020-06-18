79650b03cf5545ca836549a4abcb3293c2fe8f20
john morris <fennymorris@live.co.uk>
2020-06-18 18:09:11 +0100

    add basic fetch query

---
+++ frontend/src/components/Posts.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <h1>Apolo Client</h1>
+    <ul>
+      <li v-for="(post, index) in getPosts" :key="index">{{post}}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import { ALL_POSTS_QUERY } from '../constants/graphql'
+
+export default {
+  name: 'Posts',
+  data () {
+    return {
+      getPosts: []
+    }
+  },
+  apollo: {
+    getPosts: {
+      query: ALL_POSTS_QUERY
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,17 +1,20 @@
 <template>
   <div class="home">
     <HelloWorld msg="Home page"/>
+    <posts-vue></posts-vue>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
+import PostsVue from '../components/Posts.vue'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    HelloWorld,
+    PostsVue
   }
 }
 </script>
