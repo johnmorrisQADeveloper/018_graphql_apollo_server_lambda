07ba0cb91e7870f6a978e551f7c72cc6598ff67a
john morris <fennymorris@live.co.uk>
2020-06-19 16:58:50 +0100

    redirect and show user credentials

--- frontend/src/views/Register.vue
+++ frontend/src/views/Register.vue
@@ -1,14 +1,16 @@
 <template>
   <v-app id="inspire">
-    <v-form ref="form" v-model="valid" lazy-validation>
+    <v-form ref="form" v-show="show" v-model="valid" lazy-validation>
       <v-text-field v-model="username" label="User Name" required></v-text-field>
       <v-text-field v-model="password" label="Password" required></v-text-field>
       <v-text-field v-model="confirmPassword" label="Confirm Pasword" required></v-text-field>
       <v-text-field v-model="email" label="E-mail" required></v-text-field>
-
       <v-btn color="error" class="mr-4" @click="reset">Reset Form</v-btn>
       <v-btn :disabled="!valid" color="success" class="mr-4" @click="submit">SUBMIT</v-btn>
     </v-form>
+    <div v-show="!show">
+      {{userdetails}}
+    </div>
   </v-app>
 </template>
 
@@ -17,11 +19,13 @@ import { CREATE_USER_MUTATION } from '../constants/graphql'
 export default {
   name: 'Register',
   data: () => ({
+    userdetails: {},
     valid: false,
     username: '',
     password: '',
     confirmPassword: '',
-    email: ''
+    email: '',
+    show: true
   }),
   methods: {
     reset () {
@@ -30,9 +34,9 @@ export default {
       this.confirmPassword = ''
       this.email = ''
     },
-    createUser () {
+    async createUser () {
       const { username, password, confirmPassword, email } = this.$data
-      this.$apollo.mutate({
+      const res = await this.$apollo.mutate({
         mutation: CREATE_USER_MUTATION,
         variables: {
           username,
@@ -41,6 +45,12 @@ export default {
           email
         }
       })
+      this.userdetails.email = res.data.register.email
+      this.userdetails.token = res.data.register.token
+      this.userdetails.username = res.data.register.username
+      this.userdetails.createdAt = res.data.register.createdAt
+      this.show = false
+      this.$router.push('/')
     },
     async submit () {
       await this.createUser()
