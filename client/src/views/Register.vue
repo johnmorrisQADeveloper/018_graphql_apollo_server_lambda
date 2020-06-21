2623a84b0eb59ff51e0380648ae1447c719c5b7c
john morris <fennymorris@live.co.uk>
2020-06-21 20:28:44 +0100

    a registration form new user

--- client/src/views/Register.vue
+++ client/src/views/Register.vue
@@ -1,15 +1,103 @@
 <template>
-  <div>
-    Register
-  </div>
+  <v-app id="inspire">
+    <v-card class="mx-auto" style="width: 65%;">
+      <v-toolbar color="deep-purple accent-4" cards dark flat>
+        <v-btn icon>
+          <v-icon>mdi-arrow-left</v-icon>
+        </v-btn>
+        <v-card-title class="title font-weight-regular">Register a new user</v-card-title>
+      </v-toolbar>
+      <v-form ref="form" v-model="form" class="pa-4 pt-6">
+        <v-text-field v-model="username" filled color="deep-purple" label="User Name"></v-text-field>
+        <v-text-field
+          v-model="password"
+          filled
+          color="deep-purple"
+          label="Password"
+          style="min-height: 96px"
+          type="password"
+        ></v-text-field>
+        <v-text-field
+          v-model="confirmPassword"
+          filled
+          color="deep-purple"
+          label="Confirm Password"
+          style="min-height: 96px"
+          type="password"
+        ></v-text-field>
+        <v-text-field
+          v-model="email"
+          :rules="[rules.email]"
+          filled
+          color="deep-purple"
+          label="Email address"
+          type="email"
+        ></v-text-field>
+        <v-checkbox v-model="agreement" :rules="[rules.required]" color="deep-purple">
+          <template v-slot:label>
+            I agree to the&nbsp;
+            <a href="#" @click.stop.prevent="dialog = true">Terms of Service</a>
+            &nbsp;and&nbsp;
+            <a
+              href="#"
+              @click.stop.prevent="dialog = true"
+            >Privacy Policy</a>*
+          </template>
+        </v-checkbox>
+      </v-form>
+      <v-divider></v-divider>
+      <v-card-actions>
+        <v-btn text @click="$refs.form.reset()">Clear</v-btn>
+        <v-spacer></v-spacer>
+        <v-btn
+          :disabled="!form"
+          :loading="isLoading"
+          class="white--text"
+          color="deep-purple accent-4"
+          depressed
+        >Submit</v-btn>
+      </v-card-actions>
+      <v-dialog v-model="dialog" absolute max-width="400" persistent>
+        <v-card>
+          <v-card-title class="headline grey lighten-3">Legal</v-card-title>
+          <v-card-text>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</v-card-text>
+          <v-divider></v-divider>
+          <v-card-actions>
+            <v-btn text @click="agreement = false, dialog = false">No</v-btn>
+            <v-spacer></v-spacer>
+            <v-btn
+              class="white--text"
+              color="deep-purple accent-4"
+              @click="agreement = true, dialog = false"
+            >Yes</v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-dialog>
+    </v-card>
+  </v-app>
 </template>
 
 <script>
 export default {
-  name: 'Register'
+  name: 'Register',
+  data: () => ({
+    agreement: false,
+    username: '',
+    confirmPassword: '',
+    dialog: false,
+    email: '',
+    form: false,
+    isLoading: false,
+    password: undefined,
+    rules: {
+      email: v => (v || '').match(/@/) || 'Please enter a valid email',
+      length: len => v =>
+        (v || '').length >= len || `Invalid character length, required ${len}`,
+      password: v =>
+        (v || '').match(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*(_|[^\w])).+$/) ||
+        'Password must contain an upper case letter, a numeric character, and a special character',
+      required: v => !!v || 'This field is required'
+    }
+  })
 }
 </script>
-
-<style lang="scss" scoped>
-
-</style>
