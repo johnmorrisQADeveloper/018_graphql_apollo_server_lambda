27487c2154d299f16dee6adfa64f8e26dcf32443
john morris <fennymorris@live.co.uk>
2020-06-21 19:57:38 +0100

    add like and comment button

--- client/src/components/Posts.vue
+++ client/src/components/Posts.vue
@@ -1,31 +1,35 @@
 <template>
-  <v-container class="pink lighten-5">
+  <v-container class="lighten-5">
     <v-row>
       <h4>Recent Posts</h4>
     </v-row>
     <v-row class="d-flex align-center">
-      <v-card style="width: 30%" class="px-4 ma-2" color="#26c6da" dark v-for="(post, index) in getPosts" :key="index">
-          <v-card-title color="#086972" class="rounded-xl">
-            <v-list color="#01a9b4">
-              <v-list-item link :to="post.id">
-                <v-list-item-action>
-                  <v-icon>mdi-twitter</v-icon>
-                </v-list-item-action>
-                <v-list-item-content>
-                  <v-list-item-title>{{ dateSince(post.createdAt) }}</v-list-item-title>
-                </v-list-item-content>
-              </v-list-item>
-            </v-list>
+      <v-card
+        style="width: 30%"
+        class="px-4 ma-2"
+        color="#26c6da"
+        dark
+        v-for="(post, index) in getPosts"
+        :key="index"
+      >
+        <v-card-title color="#086972" class="rounded-xl">
+          <v-list color="#01a9b4">
+            <v-list-item link :to="post.id">
+              <v-list-item-action>
+                <v-icon>mdi-twitter</v-icon>
+              </v-list-item-action>
+              <v-list-item-content>
+                <v-list-item-title>{{ dateSince(post.createdAt) }}</v-list-item-title>
+              </v-list-item-content>
+            </v-list-item>
+          </v-list>
         </v-card-title>
         <v-card-text link :to="post.id" class="headline font-weight-bold">{{post.body}}</v-card-text>
 
         <v-card-actions>
           <v-list-item class="grow">
             <v-list-item-avatar color="grey darken-3">
-              <v-img
-                class="elevation-6"
-                src=""
-              ></v-img>
+              <v-img class="elevation-6" src></v-img>
             </v-list-item-avatar>
 
             <v-list-item-content>
@@ -33,11 +37,14 @@
             </v-list-item-content>
 
             <v-row align="center" justify="end">
-              <v-icon class="mr-1">mdi-heart</v-icon>
-              <span class="subheading mr-2">{{post.likeCount}}</span>
-              <span class="mr-1">·</span>
-              <v-icon class="mr-1">mdi-comment</v-icon>
-              <span class="subheading">{{post.commentCount}}</span>
+              <v-btn class="ma-2" tile outlined color="pink">
+                <v-icon left>mdi-heart</v-icon>
+                {{post.likeCount}}
+              </v-btn>
+              <v-btn class="ma-2" tile outlined color="success">
+                <v-icon left>mdi-comment</v-icon>
+                {{post.commentCount}}
+              </v-btn>
             </v-row>
           </v-list-item>
         </v-card-actions>
